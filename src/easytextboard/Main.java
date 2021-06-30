95198687573f14fad75bb004cabce53bb1717144
kimdohy <134ehgud@naver.com>
2021-06-30 18:28:22 +0900

    스캐너로 명령어 입력받기

--- src/easytextboard/Main.java
+++ src/easytextboard/Main.java
@@ -1,8 +1,34 @@
 package easytextboard;
+
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("안녕");
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+
+			System.out.print("명령어를 입력하시오>>");
+			String command = sc.nextLine();
+			if (command.equals("article add")) {
+				System.out.println("== 게시물 등록 ==");
+				continue;
+			}
+			if (command.equals("article list")) {
+				System.out.println("== 게시물 목록 ==");
+				continue;
+			}
+			if (command.equals("exit")) {
+				System.out.println("== 시스템 종료 ==");
+				break;
+			} else {
+				System.out.println("없는 명령어 입니다 !!");
+				continue;
+			}
+
+		}
+
 	}
 
 }
